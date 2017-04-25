755e71fe98150115f82e8599f6be7300d7c1899b
Osirisz <earthstroming@hotmail.com>
2017-04-25 23:18:25 +0700

    finish js close all file

--- test.js
+++ test.js
@@ -1,27 +1,100 @@
 function closeExtendContent() {
-  var z = document.getElementsByClassName("col-md-4 cardTestProj")[0].removeAttribute("style");
-  var aa = document.getElementsByClassName("test");
-  for (acountTest = 0; acountTest < aa.length; acountTest++) {
-      aa[acountTest].style.display = "none";
+  var a = document.getElementsByClassName("test");
+  var b = document.getElementsByClassName("mt-2 contentTestProj");
+  var c = document.getElementsByClassName("detailContentProj");
+  var d = document.getElementsByClassName("content-wrapper divTestProj");
+  var e = document.getElementsByClassName("col-md-4 cardTestProj");
+  var f = document.getElementsByClassName("mt-2 contentTestInfra");
+  var g = document.getElementsByClassName("detailContentInfra");
+  var h = document.getElementsByClassName("content-wrapper divTestInfra");
+  var i = document.getElementsByClassName("col-md-4 cardTestInfra");
+  var j = document.getElementsByClassName("mt-2 contentTestServ");
+  var k = document.getElementsByClassName("detailContentServ");
+  var l = document.getElementsByClassName("content-wrapper divTestServ");
+  var m = document.getElementsByClassName("col-md-4 cardTestServ");
+
+  var countTest;
+  for (countTest = 0; countTest < a.length; countTest++) {
+      a[countTest].style.display = "none";
+  }
+
+  var countMt2ContentTestProj;
+  for (countMt2ContentTestProj = 0; countMt2ContentTestProj < b.length; countMt2ContentTestProj++) {
+      b[countMt2ContentTestProj].removeAttribute("style");
+  }
+
+  var countDetailContentProj;
+  for (countDetailContentProj = 0; countDetailContentProj < c.length; countDetailContentProj++) {
+      c[countDetailContentProj].removeAttribute("style");
+  }
+
+  var countContentWrapperDivTestProj;
+  for (countContentWrapperDivTestProj = 0; countContentWrapperDivTestProj < d.length; countContentWrapperDivTestProj++) {
+      d[countContentWrapperDivTestProj].removeAttribute("style");
+  }
+
+  var countCol4CardTestProj;
+  for (countCol4CardTestProj = 0; countCol4CardTestProj < e.length; countCol4CardTestProj++) {
+      e[countCol4CardTestProj].removeAttribute("style");
+  }
+
+  var countMt2ContentTestInfra;
+  for (countMt2ContentTestInfra = 0; countMt2ContentTestInfra < f.length; countMt2ContentTestInfra++) {
+      f[countMt2ContentTestInfra].removeAttribute("style");
+  }
+
+  var countDetailContentInfra;
+  for (countDetailContentInfra = 0; countDetailContentInfra < g.length; countDetailContentInfra++) {
+      g[countDetailContentInfra].removeAttribute("style");
+  }
+
+  var countContentWrapperDivTestInfra;
+  for (countContentWrapperDivTestInfra = 0; countContentWrapperDivTestInfra < h.length; countContentWrapperDivTestInfra++) {
+      h[countContentWrapperDivTestInfra].removeAttribute("style");
+  }
+
+  var countCol4CardTestInfra;
+  for (countCol4CardTestInfra = 0; countCol4CardTestInfra < i.length; countCol4CardTestInfra++) {
+      i[countCol4CardTestInfra].removeAttribute("style");
+  }
+
+  var countMt2ContentTestServ;
+  for (countMt2ContentTestServ = 0; countMt2ContentTestServ < j.length; countMt2ContentTestServ++) {
+      j[countMt2ContentTestServ].removeAttribute("style");
+  }
+
+  var countDetailContentServ;
+  for (countDetailContentServ = 0; countDetailContentServ < k.length; countDetailContentServ++) {
+      k[countDetailContentServ].removeAttribute("style");
+  }
+
+  var countContentWrapperDivTestServ;
+  for (countContentWrapperDivTestServ = 0; countContentWrapperDivTestServ < l.length; countContentWrapperDivTestServ++) {
+      l[countContentWrapperDivTestServ].removeAttribute("style");
+  }
+
+  var countCol4CardTestInServ;
+  for (countCol4CardTestInServ = 0; countCol4CardTestInServ < m.length; countCol4CardTestInServ++) {
+      m[countCol4CardTestInServ].removeAttribute("style");
   }
 }
 function extendContent() {
     var a = document.getElementsByClassName("test");
-    var b = document.getElementsByClassName("topic");
-    var c = document.getElementsByClassName("detailContentBut");
-    var d = document.getElementsByClassName("mt-2 contentTestProj");
-    var e = document.getElementsByClassName("detailContentProj");
-    var f = document.getElementsByClassName("content-wrapper divTestProj");
-    var g = document.getElementsByClassName("col-md-4 cardTestProj");
-    var h = document.getElementsByClassName("mt-2 contentTestInfra");
-    var i = document.getElementsByClassName("detailContentInfra");
-    var j = document.getElementsByClassName("content-wrapper divTestInfra");
-    var k = document.getElementsByClassName("col-md-4 cardTestInfra");
-    var l = document.getElementsByClassName("mt-2 contentTestServ");
-    var m = document.getElementsByClassName("detailContentServ");
-    var n = document.getElementsByClassName("content-wrapper divTestServ");
-    var o = document.getElementsByClassName("col-md-4 cardTestServ");
-
+    var b = document.getElementsByClassName("mt-2 contentTestProj");
+    var c = document.getElementsByClassName("detailContentProj");
+    var d = document.getElementsByClassName("content-wrapper divTestProj");
+    var e = document.getElementsByClassName("col-md-4 cardTestProj");
+    var f = document.getElementsByClassName("mt-2 contentTestInfra");
+    var g = document.getElementsByClassName("detailContentInfra");
+    var h = document.getElementsByClassName("content-wrapper divTestInfra");
+    var i = document.getElementsByClassName("col-md-4 cardTestInfra");
+    var j = document.getElementsByClassName("mt-2 contentTestServ");
+    var k = document.getElementsByClassName("detailContentServ");
+    var l = document.getElementsByClassName("content-wrapper divTestServ");
+    var m = document.getElementsByClassName("col-md-4 cardTestServ");
+    var n = document.getElementById("graphTest");
+    n.style.width = "850px";
+    
     var countTest;
     for (countTest = 0; countTest < a.length; countTest++) {
         a[countTest].style.width = "500px";
@@ -35,86 +108,75 @@ function extendContent() {
         a[countTest].style.display = "block";
     }
 
-    var countTopic;
-    for (countTopic = 0; countTopic < b.length; countTopic++) {
-        b[countTopic].style.color = "grey";
-        b[countTopic].style.fontSize = "22px";
-    }
-
-    var countDetailContentBut;
-    for (countDetailContentBut = 0; countDetailContentBut < c.length; countDetailContentBut++) {
-        c[countDetailContentBut].style.marginRight = "30px";
-    }
-
     var countMt2ContentTestProj;
-    for (countMt2ContentTestProj = 0; countMt2ContentTestProj < d.length; countMt2ContentTestProj++) {
-        d[countMt2ContentTestProj].style.marginLeft = "80px";
-        d[countMt2ContentTestProj].style.width = "100%";
-        d[countMt2ContentTestProj].style.position = "relative";
+    for (countMt2ContentTestProj = 0; countMt2ContentTestProj < b.length; countMt2ContentTestProj++) {
+        b[countMt2ContentTestProj].style.marginLeft = "80px";
+        b[countMt2ContentTestProj].style.width = "100%";
+        b[countMt2ContentTestProj].style.position = "relative";
     }
 
     var countDetailContentProj;
-    for (countDetailContentProj = 0; countDetailContentProj < e.length; countDetailContentProj++) {
-        e[countDetailContentProj].style.margin = "20px";
-        e[countDetailContentProj].style.overflowY = "auto";
-        e[countDetailContentProj].style.height = "450px";
+    for (countDetailContentProj = 0; countDetailContentProj < c.length; countDetailContentProj++) {
+        c[countDetailContentProj].style.margin = "20px";
+        c[countDetailContentProj].style.overflowY = "auto";
+        c[countDetailContentProj].style.height = "450px";
     }
 
     var countContentWrapperDivTestProj;
-    for (countContentWrapperDivTestProj = 0; countContentWrapperDivTestProj < f.length; countContentWrapperDivTestProj++) {
-        f[countContentWrapperDivTestProj].style.width = "900px";
+    for (countContentWrapperDivTestProj = 0; countContentWrapperDivTestProj < d.length; countContentWrapperDivTestProj++) {
+        d[countContentWrapperDivTestProj].style.width = "900px";
     }
 
     var countCol4CardTestProj;
-    for (countCol4CardTestProj = 0; countCol4CardTestProj < g.length; countCol4CardTestProj++) {
-        g[countCol4CardTestProj].style.width = "50%";
+    for (countCol4CardTestProj = 0; countCol4CardTestProj < e.length; countCol4CardTestProj++) {
+        e[countCol4CardTestProj].style.width = "50%";
     }
 
     var countMt2ContentTestInfra;
-    for (countMt2ContentTestInfra = 0; countMt2ContentTestInfra < h.length; countMt2ContentTestInfra++) {
-        h[countMt2ContentTestInfra].style.marginLeft = "80px";
-        h[countMt2ContentTestInfra].style.width = "100%";
-        h[countMt2ContentTestInfra].style.position = "relative";
+    for (countMt2ContentTestInfra = 0; countMt2ContentTestInfra < f.length; countMt2ContentTestInfra++) {
+        f[countMt2ContentTestInfra].style.marginLeft = "80px";
+        f[countMt2ContentTestInfra].style.width = "100%";
+        f[countMt2ContentTestInfra].style.position = "relative";
     }
 
     var countDetailContentInfra;
-    for (countDetailContentInfra = 0; countDetailContentInfra < i.length; countDetailContentInfra++) {
-        i[countDetailContentInfra].style.margin = "20px";
-        i[countDetailContentInfra].style.overflowY = "auto";
-        i[countDetailContentInfra].style.height = "450px";
+    for (countDetailContentInfra = 0; countDetailContentInfra < g.length; countDetailContentInfra++) {
+        g[countDetailContentInfra].style.margin = "20px";
+        g[countDetailContentInfra].style.overflowY = "auto";
+        g[countDetailContentInfra].style.height = "450px";
     }
 
     var countContentWrapperDivTestInfra;
-    for (countContentWrapperDivTestInfra = 0; countContentWrapperDivTestInfra < j.length; countContentWrapperDivTestInfra++) {
-        j[countContentWrapperDivTestInfra].style.width = "900px";
+    for (countContentWrapperDivTestInfra = 0; countContentWrapperDivTestInfra < h.length; countContentWrapperDivTestInfra++) {
+        h[countContentWrapperDivTestInfra].style.width = "900px";
     }
 
     var countCol4CardTestInfra;
-    for (countCol4CardTestInfra = 0; countCol4CardTestInfra < k.length; countCol4CardTestInfra++) {
-        k[countCol4CardTestInfra].style.width = "50%";
+    for (countCol4CardTestInfra = 0; countCol4CardTestInfra < i.length; countCol4CardTestInfra++) {
+        i[countCol4CardTestInfra].style.width = "50%";
     }
 
     var countMt2ContentTestServ;
-    for (countMt2ContentTestServ = 0; countMt2ContentTestServ < l.length; countMt2ContentTestServ++) {
-        l[countMt2ContentTestServ].style.marginLeft = "80px";
-        l[countMt2ContentTestServ].style.width = "100%";
-        l[countMt2ContentTestServ].style.position = "relative";
+    for (countMt2ContentTestServ = 0; countMt2ContentTestServ < j.length; countMt2ContentTestServ++) {
+        j[countMt2ContentTestServ].style.marginLeft = "80px";
+        j[countMt2ContentTestServ].style.width = "100%";
+        j[countMt2ContentTestServ].style.position = "relative";
     }
 
     var countDetailContentServ;
-    for (countDetailContentServ = 0; countDetailContentServ < m.length; countDetailContentServ++) {
-        m[countDetailContentServ].style.margin = "20px";
-        m[countDetailContentServ].style.overflowY = "auto";
-        m[countDetailContentServ].style.height = "450px";
+    for (countDetailContentServ = 0; countDetailContentServ < k.length; countDetailContentServ++) {
+        k[countDetailContentServ].style.margin = "20px";
+        k[countDetailContentServ].style.overflowY = "auto";
+        k[countDetailContentServ].style.height = "450px";
     }
 
     var countContentWrapperDivTestServ;
-    for (countContentWrapperDivTestServ = 0; countContentWrapperDivTestServ < n.length; countContentWrapperDivTestServ++) {
-        n[countContentWrapperDivTestServ].style.width = "900px";
+    for (countContentWrapperDivTestServ = 0; countContentWrapperDivTestServ < l.length; countContentWrapperDivTestServ++) {
+        l[countContentWrapperDivTestServ].style.width = "900px";
     }
 
     var countCol4CardTestInServ;
-    for (countCol4CardTestInServ = 0; countCol4CardTestInServ < o.length; countCol4CardTestInServ++) {
-        o[countCol4CardTestInServ].style.width = "50%";
+    for (countCol4CardTestInServ = 0; countCol4CardTestInServ < m.length; countCol4CardTestInServ++) {
+        m[countCol4CardTestInServ].style.width = "50%";
     }
 }
